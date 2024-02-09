ddcb3310e0d7e66a8935bbfd0347dfd850af3ab1
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2024-02-09 01:08:07 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
 from setuptools import setup
 
+long_description = """
+CLCTool - Custom Linux Configuration Tool
+=========================================
+
+CLCTool is a flexible and modular command-line utility for customizing and configuring Linux systems. It allows users to define and execute tasks, install packages, enable services, configure firewalls, and more, all through a simple and scalable configuration file.
+
+Features:
+- Modular design with user-defined functions (UDFs) for advanced customization.
+- Task dependencies and conditional execution for a tailored installation process.
+- Interactive prompts for user input during the configuration.
+- Support for a scalable module system, allowing users to create and include custom modules.
+
+Installation:
+    pip install clctool
+
+Usage:
+    clctool -m module1.spoink,module2.spoink -o module1,module2 -p your_profile -v 1.0
+"""
+
 setup(
     name='CLCTool',
     version='0.1',
@@ -12,12 +31,6 @@ setup(
             'clctool = clctool:main',
         ],
     },
-    long_description="""
-    CLCTool - Custom Linux configuration tool
-    ========================================
-
-    Little but powerful.
-
-    """,
+    long_description=long_description,
     long_description_content_type='text/x-rst',
 )
